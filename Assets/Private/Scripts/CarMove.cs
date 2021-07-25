f66e25ea9c455ac64ba7d26b34d70a3b05ea663a
UMA <ume0kanaz@gmail.com>
2021-07-25 10:35:10 +0900

    implement Restart

--- Assets/Private/Scripts/CarMove.cs
+++ Assets/Private/Scripts/CarMove.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class CarMove : MonoBehaviour
 {
+    // 初期位置
+    [SerializeField] private Transform initTransform;
+
+    // 移動
     [SerializeField] private List<AxleInfo> axleInfos; 
     [SerializeField] private float maxMotorTorque; 
 
     [SerializeField] private float maxSteeringAngle; 
 
+    // 復帰
+    private bool isReadyRestart = true;
+
+    private void Start()
+    {
+        transform.position = initTransform.position;
+        transform.rotation = initTransform.rotation;
+    }
+
     private void FixedUpdate()
     {
         float motor = maxMotorTorque * Input.GetAxis("Vertical");
- 
         float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
  
         foreach (AxleInfo axleInfo in axleInfos)
@@ -28,6 +40,20 @@ public class CarMove : MonoBehaviour
                 axleInfo.rightWheel.motorTorque = motor;
             }
         }
+
+        // 復帰
+        if (Input.GetKey(KeyCode.R) && isReadyRestart)
+        {
+            isReadyRestart = false;
+            transform.position = initTransform.position;
+            transform.rotation = initTransform.rotation;
+            Invoke("SetIsReadyRestart", 3.0f);
+        }
+    }
+
+    private void SetIsReadyRestart()
+    {
+        isReadyRestart = true;
     }
 }
 
